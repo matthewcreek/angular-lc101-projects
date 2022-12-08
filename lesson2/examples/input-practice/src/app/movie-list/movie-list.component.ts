6161116b38095cb18ce0a71829240cbba833c066
Matthew Creek <matthew.creek27@gmail.com>
2022-12-07 19:28:52 -0600

    added code for input practice

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,26 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   // addMovie(newTitle: string) {
+   //    if (newTitle === '') {
+   //       return 'empty';
+   //    } else if (!this.movies.includes(newTitle)){
+   //       this.movies.push(newTitle);
+   //    } else if (this.movies.includes(newTitle)){
+   //       return 'repeat';
+   //    }
+   // }
+   
+   addMovie(newTitle: string) {
+      let error = '';
+      if(newTitle === ''){
+         error = 'Please enter a movie title.';
+      } else if (this.movies.includes(newTitle)){
+         error = `${newTitle} is already in your movie list.`;
+      } else {
+         this.movies.push(newTitle);
+      }
+      return error;
+   }
 }
