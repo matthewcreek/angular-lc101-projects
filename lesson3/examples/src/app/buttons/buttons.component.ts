0f94244509089571df58b982d95536ac778633a5
Matthew Creek <matthew.creek27@gmail.com>
2022-12-13 19:59:06 -0600

    working on bonus missions

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -6,11 +6,27 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
+   buttonHeading: string = "Buttons";
+   jokeHeading: string = 'Joke Buttons'
    inactive: boolean = false;
+   goldButton: boolean = false;
+   silverButton: boolean = false;
+   copperButton: boolean = false;
+   jokeButton: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
 
+   reactivate():void {
+      if (this.goldButton){
+         this.goldButton = false;
+      }
+      if (this.silverButton){
+         this.silverButton = false;
+      }
+      if (this.copperButton){
+         this.copperButton = false;
+      }
+   }
 }
