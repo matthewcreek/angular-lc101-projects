79ccb5851c1ba74d84ff5e38453aa6f3d772716c
Matthew Creek <matthew.creek27@gmail.com>
2022-12-14 00:16:36 -0600

    added functionality for dynamic buttons based on user interaction with rocket image

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -8,19 +8,42 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Exercises: Angular Lesson 3';
   flightStatus = 'Space shuttle ready for takeoff!';
-
+   readyLiftoff: boolean = true;
+   leftBorder = true;
+   rightBorder = true;
+   topBorder = true;
+   bottomBorder = true;
 
   color = 'green';
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
 
+  borderPolice(width, height){
+    if (width <= -20000 || width >= 170000 || height <= 0 || height >=340000){
+      this.color = 'orange'
+    } else this.color = 'blue';
+    if (width <= -20000){
+      this.leftBorder = false;
+    } else this.leftBorder = true;
+    if (width >= 170000){
+      this.rightBorder = false;
+    } else this.rightBorder = true;
+    if (height <= 0){
+      this.bottomBorder = false;
+    } else this.bottomBorder = true;
+    if (height >= 340000){
+      this.topBorder = false;
+    } else this.topBorder = true;
+  }
+
   verifyLaunch() {
     let launchReady = confirm('Proceed to launch?');
     if (launchReady){
       this.flightStatus = 'Shuttle in flight.';
       this.color = 'blue';
-      this.height += 10000
+      this.height += 10000;
+      this.readyLiftoff = false
     }
   }
   alertLand(rocketImage){
@@ -29,6 +52,7 @@ export class AppComponent {
     this.color = 'green';
     this.height = 0;
     rocketImage.style.bottom = '0px';
+    this.readyLiftoff = true;
   }
   abort(rocketImage){
     let abortReady = confirm('Proceed with abort process?');
@@ -38,13 +62,15 @@ export class AppComponent {
       this.height = 0;
       rocketImage.style.bottom = '0px';
       rocketImage.style.left = '0px';
+      this.readyLiftoff = true;
     }
   }
   moveRocket(rocketImage, direction){
     if (direction === 'right'){
       let movement = parseInt(rocketImage.style.left) + 10 + 'px';
       rocketImage.style.left = movement;
-      this.width = this.width + 10000
+      this.width = this.width + 10000;
+      console.log(movement)
     }
     if (direction === 'left'){
       let movement = parseInt(rocketImage.style.left) - 10 + 'px';
@@ -59,8 +85,9 @@ export class AppComponent {
     if (direction === 'down'){
       let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
       rocketImage.style.bottom = movement;
-      this.height = this.height + 10000;
+      this.height = this.height - 10000;
     }
     console.log(this.width)
+    this.borderPolice(this.width, this.height)
   }
 }
